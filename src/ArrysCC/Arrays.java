8179b203bc4a0e8d989b77b9a0165d6adf7443a7
Daniel Díaz González <d.g.daniel@outlook.com>
2020-11-29 11:17:58 +0100

    Arrays finalizado

--- src/ArrysCC/Arrays.java
+++ src/ArrysCC/Arrays.java
@@ -3,7 +3,7 @@
  */
 package ArrysCC;
 
-import Utilidades.Numeros;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -47,15 +47,123 @@ public class Arrays {
                     break;
                 case 2:
                     correcto = false;
-
+                    boolean encontrado = false;
+                    int numero = 0;
+                    int aleatorio[] = new int[10];
+                    //Creamos una matriz aleatoriade hasta el numero 20
+                    for (int i = 0; i < 10; i++) {
+                        aleatorio[i] = (int) Math.round(Math.random() * 20);
+                    }
+                    //Repetimos que recorra el arrays hasta que encontremos un numero acertado
+                    do {
+                        System.out.print("Elije un número: ");
+                        //capturamos exception
+                        try {
+                            numero = entrada.nextInt();
+                        } catch (InputMismatchException e) {
+                            System.out.println("Escribe un número entero");
+                        }
+                        entrada.nextLine();//limpiamos buffer
+                        for (int i = 0; i < 10; i++) {
+                            if (aleatorio[i] == numero) {
+                                System.out.println("Has acertado!!");
+                                //Aqui falla si le incorporo el numero que hemos puesto
+                                System.out.println("El número se encuentra en la posición: " + i);
+                                encontrado = true;
+                            }
+                        }
+                    } while (encontrado == false);//Cuidado he fallado puse "=" y no "=="
                     break;
                 case 3:
                     correcto = false;
 
+                    int tamaño = 0;
+
+                    do {
+                        try {
+                            System.out.print("Elije el tamaño de un Array: ");
+                            tamaño = entrada.nextInt();
+                            correcto = true;
+                        } catch (InputMismatchException e) {
+                            System.out.println("Solo puedes indicar un número entero");
+                        }
+                        entrada.nextLine();
+                    } while (correcto == false);
+                    double[] aleatorio2 = new double[tamaño];
+                    for (int i = 0; i < tamaño; i++) {
+                        aleatorio2[i] = Math.random() * 100;
+                    }
+                    //Mostramos el array
+                    for (double i : aleatorio2) {
+                        System.out.println(i);
+                    }
+                    //Oredenamos el array de menor a mayor
+                    for (int i = 0; i < tamaño - 1; i++) {
+                        for (int j = 1; j < (tamaño - i); j++) {
+                            if (aleatorio2[j - 1] > aleatorio2[j]) {   // si el elemento anterior es mayor, hacemos el cambio
+                                double aux_elem = aleatorio2[j];
+                                aleatorio2[j] = aleatorio2[j - 1];
+                                aleatorio2[j - 1] = aux_elem;
+                            }
+                        }
+                    }
+                    //Menor
+                    System.out.println("El número menor es: " + aleatorio2[0]);
+                    //Mayor
+                    System.out.println("El número mayor es: " + aleatorio2[tamaño - 1]);
+                    //Media
+                    double suma = 0;
+                    double media = 0;
+                    for (int i = 0; i < tamaño; i++) {
+                        suma += aleatorio2[i];
+                        media = suma / 2;
+                    }
+                    System.out.println("La media de la matriz es: " + media);
+                    //Lo volvemos a poner en false para que no nos saque del menu de array
+                    correcto = false;
                     break;
                 case 4:
                     correcto = false;
 
+                    int tamaño2 = 0;
+
+                    do {
+                        try {
+                            System.out.print("Elije el tamaño de un Array: ");
+                            tamaño2 = entrada.nextInt();
+                            correcto = true;
+                        } catch (InputMismatchException e) {
+                            System.out.println("Solo puedes indicar un número entero");
+                        }
+                        entrada.nextLine();
+                    } while (correcto == false);
+
+                    double[] aleatorio3 = new double[tamaño2];
+                    for (int i = 0; i < tamaño2; i++) {
+                        aleatorio3[i] = Math.random() * 100;
+                    }
+                    //Mostramos el array con un bucle for each
+                    for (double i : aleatorio3) {
+                        System.out.println(i);
+                    }
+                    //Oredenamos el array de menor a mayor
+                    for (int i = 0; i < tamaño2 - 1; i++) {
+                        for (int j = 1; j < (tamaño2 - i); j++) {
+                            if (aleatorio3[j - 1] > aleatorio3[j]) {   // si el elemento anterior es mayor, hacemos el cambio
+                                double aux_elem = aleatorio3[j];
+                                aleatorio3[j] = aleatorio3[j - 1];
+                                aleatorio3[j - 1] = aux_elem;
+                            }
+                        }
+                    }
+                    System.out.println("Los números mayoes a 50 son:");
+                    //Recorremos el array con un bucle de nuevo y mostramos que valores de la matriz son mayores a 50
+                    for (int i = 0; i < tamaño2; i++) {
+                        if (aleatorio3[i] > 50) {
+                            System.out.println(aleatorio3[i]);
+                        }
+                    }
+                    correcto = false;
                     break;
                 case 0:
                     break;
