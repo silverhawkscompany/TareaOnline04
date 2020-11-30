fc104def02bc6754c8f8fa2c3be726d9a4f1a4f2
Daniel Díaz González <d.g.daniel@outlook.com>
2020-11-30 12:10:57 +0100

    Validar cadenas finalizado

--- src/HundirLaFlota/HundirFlota.java
+++ src/HundirLaFlota/HundirFlota.java
@@ -9,4 +9,6 @@ package HundirLaFlota;
  */
 public class HundirFlota {
 
+    int[][] jugador1 = new int[10][10];
+    int[][] jugador2 = new int[10][10];
 }
--- src/Utilidades/ValidarCadenas.java
+++ src/Utilidades/ValidarCadenas.java
@@ -3,7 +3,9 @@
  */
 package Utilidades;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -34,10 +36,12 @@ public class ValidarCadenas {
                 case 1:
                     correcto = false;
                     comprobarMatriculaAutomovil();
+
                     break;
                 case 2:
                     correcto = false;
                     comprobarMatriculaCiclomotor();
+
                     break;
                 case 0:
                     break;
@@ -46,10 +50,40 @@ public class ValidarCadenas {
     }
 
     private static void comprobarMatriculaAutomovil() {
+        //4 números seguida de 3 letras. Ejemplo: 4231BBC
+        Scanner entrada = new Scanner(System.in);
+        String matricula;
+
+        System.out.println("Escribe la matricula del automóvil: ");
+        matricula = entrada.nextLine();
+
+        Pattern p = Pattern.compile("[0-9]{4}[a-zA-Z]{3}");
+        Matcher m = p.matcher(matricula);
+
+        if (m.matches()) {
+            System.out.println("Matrícula correcta");
+        } else {
+            System.out.println("La matrícula no es correcta");
+        }
 
     }
 
     private static void comprobarMatriculaCiclomotor() {
+        //1 letra seguida de 4 números y, posteriormente, 3 letras más. Ejemplo: C8731BTJ
+        Scanner entrada = new Scanner(System.in);
+        String matricula;
+
+        System.out.println("Escribe la matricula del ciclomotor: ");
+        matricula = entrada.nextLine();
+
+        Pattern p = Pattern.compile("[a-zA-Z]{1}[0-9]{4}[a-zA-Z]{3}");
+        Matcher m = p.matcher(matricula);
+
+        if (m.matches()) {
+            System.out.println("Matrícula correcta");
+        } else {
+            System.out.println("La matrícula no es correcta");
+        }
 
     }
 }
